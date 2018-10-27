311eff942a89390d85c39cfe8553be53d7f55c26
andres <andreswang0703@gmail.com>
2018-10-27 14:41:20 -0400

    Leetcode No.22 generate parentheses

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -1,12 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Leetcode No.22 (medium): generate parentheses
+ * 
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * For example, given n = 3, a solution set is:
+ * [
+ *  "((()))",
+ *  "(()())",
+ *  "(())()",
+ *  "()(())",
+ *  "()()()"
+ * ]
+ * 
+ * @author andres
+ * @date 10/27/2018
+ *
+ */
 public class GenerateParentheses {
     
-    /*
-    public static List<String> generateParentheses(int n) {
-        int countA = 0, countB = 0;
-        String res = "";
-        
+	// recursive backtracking method
+    public List<String> generateParentheses(int n) {
+        List<String> result = new ArrayList<String>();
+    	backtrack(result, "", n, 0, 0);
+        return result;
     }
-    */
-
+    
+    private void backtrack(List<String> res, String cur, int max, int open, int close) {
+    	if (cur.length() == 2 * max) {
+    		res.add(cur);
+    		return;
+    	}
+    	
+    	if (open < max) {
+    		backtrack(res, cur + "(", max, open + 1, close);
+    	}
+    	if (close < open) {
+    	    backtrack(res, cur + ")", max, open, close + 1);
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	GenerateParentheses gp = new GenerateParentheses();
+    	System.out.println(gp.generateParentheses(5));
+    }
 }
